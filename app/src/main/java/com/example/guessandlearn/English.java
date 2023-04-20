4f0cb6f96c3804f6789455711d3b01ab95482c80
ArthDimaano <131332012+ArthDimaano@users.noreply.github.com>
2023-04-20 21:05:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/guessandlearn/English.java
@@ -0,0 +1,24 @@
+package com.example.guessandlearn;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.guessandlearn.databinding.FragmentSecondBinding;
+
+
+public class English extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_english, container, false);
+    }
+}
