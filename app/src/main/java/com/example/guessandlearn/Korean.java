57a68a415cbc95393261c128f380ea3b3c088e1f
Gagocode <96364834+Gagocode@users.noreply.github.com>
2023-04-20 22:23:18 +0800

    tangina nyo inayus ko na ha

--- app/src/main/java/com/example/guessandlearn/Korean.java
+++ app/src/main/java/com/example/guessandlearn/Korean.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class English extends AppCompatActivity {
+public class Korean extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_english);
+        setContentView(R.layout.activity_korean);
     }
 }
---
+++ app/src/main/java/com/example/guessandlearn/cathegories.java
@@ -0,0 +1,71 @@
+package com.example.guessandlearn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class cathegories extends AppCompatActivity {
+
+    private Button english, korean, japanese, thai;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cathegories);
+
+        english = (Button) findViewById(R.id.button_english);
+        english.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openEnglish();
+            }
+        });
+
+        korean = (Button) findViewById(R.id.button_korean);
+        korean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openKorean();
+            }
+        });
+
+        japanese = (Button) findViewById(R.id.button_japanese);
+        japanese.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openJapanese();
+            }
+        });
+
+        thai = (Button) findViewById(R.id.button_thai);
+        thai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openThai();
+            }
+        });
+
+    }
+
+    private void openThai() {
+        Intent intent = new Intent(this, Thai.class);
+        startActivity(intent);
+    }
+
+    private void openJapanese() {
+        Intent intent = new Intent(this, Japanese.class);
+        startActivity(intent);
+    }
+
+    private void openKorean() {
+        Intent intent = new Intent(this, Korean.class);
+        startActivity(intent);
+    }
+
+    private void openEnglish() {
+        Intent intent = new Intent(this, English.class);
+        startActivity(intent);
+    }
+}
